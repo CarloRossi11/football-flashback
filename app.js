c76a6666da4e19bf4b7da68829b13fb03f3f859f
Carlo Berardelli <owner@Carlos-MBP.fios-router.home>
2020-08-24 20:59:29 -0400

    able to pull specific game data

---
+++ app.js
@@ -0,0 +1,30 @@
+$('form').on('submit', (event) => {
+    event.preventDefault()
+
+    // const team = $('input').val();
+
+    $.ajax({
+      url: `https://www.scorebat.com/video-api/v1/`,
+    }).then((data) => {
+      console.log(data[50]);
+      $('#match').html(data[50].title)
+      $('#comp').html(data[50].competition.name)
+      $('#date').html(data[50].date)
+      $('#video').html(data[50].url)
+    
+    },
+    () => {
+        console.log('bad request')
+    });
+})
+
+// `http://omdbapi.com/?apikey=53aa2cd6&t=${movie}`
+
+// TO DO: \\
+// Make input field responsive
+// Make video URL provide a preview, or at least be a clickable link
+// Style the results better
+// 
+
+
+
